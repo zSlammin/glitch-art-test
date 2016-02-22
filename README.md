1dc28cd40048aee4fe6c3fe8daddd48e37b51ae1
Zachary Salmon <zsalmon@Zacharys-MacBook-Pro.local>
2016-02-21 20:11:34 -0500

    byte degredation over time

---
+++ README.md
@@ -0,0 +1,6 @@
+#Glitch Art Sandbox
+
+Playing around with glitch art stuff in node.js 
+
+Using jhuckaby's [Webcam.js](https://github.com/jhuckaby/webcamjs) for the webcam functionality. and snorpey's (glitch canvas)[https://github.com/snorpey/glitch-canvas] for some random byte changing stuff. 
+
--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var express = require('express');
 var fs = require('fs')
 var app = express();
 var http= require('http');
-var https = require('https');
+// var https = require('https');
 app.use(express.static('static'))
 
 
-var options = {
-	key: fs.readFileSync('key.pem'),
-	cert: fs.readFileSync('cert.pem')
-}
+// var options = {
+// 	key: fs.readFileSync('key.pem'),
+// 	cert: fs.readFileSync('cert.pem')
+// }
 http.createServer(app).listen(8000, function(){
 	console.log("listening on 8000")
 })
-https.createServer(options, app).listen(4430, function(){
-	console.log("securely listening on 4430")
-});
+// https.createServer(options, app).listen(4430, function(){
+// 	console.log("securely listening on 4430")
+// });
